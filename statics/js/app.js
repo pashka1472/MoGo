db0f68fb4700362ee2b018c92ed12ce915b44af9
pashka1472 <pavel.bobreshov@gmail.com>
2023-11-24 09:48:43 -0500

    first commit

---
+++ statics/js/app.js
@@ -0,0 +1,74 @@
+$(function(){
+
+
+   
+   var header = $("#header"),
+        introH=$("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+        /*fixed header*/
+        $(window).on("scroll", function(){
+            scrollOffset = $(this).scrollTop();
+
+            checkScroll(scrollOffset);
+        });
+
+        function checkScroll(scrollOffset) {
+            if(scrollOffset >= introH) {
+                header.addClass("fixed");
+            } else {
+                header.removeClass("fixed");
+
+            }
+        }
+
+        /*smooth scroll*/
+
+        $("[data-scroll]").on("click", function(event) {
+            event.preventDefault();
+
+            var 
+                $this = $(this)
+                blockId = $this.data("scroll"),
+                blockOffset = $(blockId).offset().top;
+            
+            $("#nav a").removeClass("active");
+            $this.addClass("active");
+
+            $("html,body").animate({
+                scrollTop: blockOffset
+            }, 500);
+
+        });
+
+        /*menu nav_toggle*/
+
+        $("#nav_toggle").on('click', function(event) {
+            event.preventDefault();
+            $(this).toggleClass("active");
+            $('#nav').toggleClass('active');
+        });
+
+
+        /*Collapse*/
+
+        $("[data-collapse]").on("click", function(event) {
+            event.preventDefault();
+
+            var $this = $(this),
+                blockId = $this.data("collapse");
+
+            $this.toggleClass("active");
+
+
+        });
+
+        /*slider*/
+        $("[data-slider]").slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll:1,
+            fade:false
+        });
+
+});
